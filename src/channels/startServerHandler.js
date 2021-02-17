a5f7f454feeb035af2e0d51bc64552f712495878
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2021-02-17 16:27:17 +0100

    fix: move done event handling on the top level

--- src/channels/startServerHandler.js
+++ src/channels/startServerHandler.js
@@ -5,20 +5,27 @@ import newMessagesIterator from './newMessagesIterator.js';
 export default async function startServerHandler({ channel, getMethod, unhandled }) {
   let channelClosed = false;
   const [newReader, newWriter] = newInputOutput(channel, unhandled);
+  const notifications = {};
+  // Listen for the "done" event sent by the client part to finish the call.
+  // So the server-side part should stop to send data.
+  const reg = channel.handle('done', (options = {}) => {
+    const f = notifications[options.callId];
+    f && f();
+    return { code: 200 };
+  })
+
   async function handleCall({ callId, packageName, serviceName, methodName }) {
-    const method = getMethod({ packageName, serviceName, methodName });
-    const request = newReader(callId);
-    // Listen for the "done" event sent by the client part to finish the call.
-    // So the server-side part should stop to send data.
-    let stop = false;
-    const reg = channel.handle('done', (options) => {
-      if (options.callId !== callId) return;
-      stop = true;
-      reg();
-    })
-    const f = interrupt(() => channelClosed || stop);
-    const response = f(method(request));
-    await newWriter(callId, response);
+    try {
+      let stop = false;
+      notifications[callId] = () => stop = true;
+      const method = getMethod({ packageName, serviceName, methodName });
+      const request = newReader(callId);
+      const f = interrupt(() => channelClosed || stop);
+      const response = f(method(request));
+      await newWriter(callId, response);
+    } finally {
+      delete notifications[callId];
+    }
   }
   try {
     for await (let callInfo of newMessagesIterator(channel, 'init')) {
@@ -26,5 +33,7 @@ export default async function startServerHandler({ channel, getMethod, unhandled
     }
   } finally {
     channelClosed = true;
+    Object.values(notifications).forEach(r => r && r());
+    reg();
   }
 }
